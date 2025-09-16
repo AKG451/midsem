0192594ebc0ca2f99dfb0e4b96e16e88d69c85e4
AKG451 <akg451154@gmail.com>
2025-09-16 14:28:30 +0530

    Added basic file for tax calculation program

---
+++ tax.c
@@ -0,0 +1,10 @@
+//Tax calculation program
+//1. For income up to Rs. 2,50,000: No tax
+//2. For income Rs. 2,50,001 to Rs. 5,00,000: 5% on the income exceeding Rs. 2,50,000
+//3. For income Rs. 5,00,001 to Rs. 10,00,000: 20% on the income exceeding Rs. 5,00,000
+//4. For income above Rs. 10,00,000: 30% on the income exceeding Rs. 10,00,000
+#include <stdio.h>
+
+int main(){
+
+}
