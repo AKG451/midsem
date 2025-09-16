0520e1b02b82cd9f5fc195f57448fbf39e27e5b3
AKG451 <akg451154@gmail.com>
2025-09-17 02:16:46 +0530

    added something

---
+++ bubbleprac.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Print the unsorted array
+    printf("Unsorted array: \n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    for(int i = 0;i<size-1;i++){
+        for(int j = 0;j<size-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+      }
+    }
+
+     printf("Sorted array (Bubble Sort): \n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
